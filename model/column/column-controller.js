25e66d4092c23ef9e1d7b2e0e2e7e39338a35c0e
kostia777999 <kostya.gorozhanov@mail.ru>
2017-01-12 22:38:03 +0200

    update array

--- model/column/column-controller.js
+++ model/column/column-controller.js
@@ -4,15 +4,15 @@ const mongoose = require('mongoose');
 
 class ColumnController extends Controller {
     updateAll(req, res, next) {
-        let query = {
-            _id: {
-                $in: req.body.map(item => mongoose.Types.ObjectId(item._id))
-            }
-        }
-        return this.model.find(query)
-            .then(collection => res.status(200).json(collection))
-            .catch(err => next(err));
+        async.eachSeries(req.body, function (column, asyncdone) {
+            this.model.update({_id: column._id}, column)
+                .then(asyncdone);
+        }, function (err) {
+            if (err) next(err)
+             res.status(200).end();
+        });
     }
+
 }
 
 module.exports = new ColumnController(Column);
