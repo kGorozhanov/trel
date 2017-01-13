7c97a72e5b6837c1698da5532962ebdd07ba78b9
kostia777999 <kostya.gorozhanov@mail.ru>
2017-01-13 17:40:18 +0200

    enable index updates for tasks

--- model/task/task-controller.js
+++ model/task/task-controller.js
@@ -4,17 +4,16 @@ var async = require('async');
 var mongoose = require('mongoose');
 
 class TaskController extends Controller {
-    // updateAllIndexes(req, res, next) {
-    //     async.eachSeries(req.body, (column, asyncdone) => {
-    //         this.model.update({_id: mongoose.Types.ObjectId(column._id)}, {index: column.index})
-    //             .then(result => asyncdone())
-    //             .catch(err => asyncdone(null, err));
-    //     }, (err) => {
-    //         if (err) next(err)
-    //          res.status(200).end();
-    //     });
-    // }
-
+    updateAllIndexes(req, res, next) {
+        async.eachSeries(req.body, (task, asyncdone) => {
+            this.model.update({_id: mongoose.Types.ObjectId(task._id)}, {index: task.index, column: task.column})
+                .then(result => asyncdone())
+                .catch(err => asyncdone(null, err));
+        }, (err) => {
+            if (err) next(err)
+             res.status(200).end();
+        });
+    }
 }
 
 module.exports = new TaskController(Task);
